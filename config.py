b638b7919b759923b7a074a3a36c41e3974439b0
itsmanhwalibrary <itsmanhwalibrary@gmail.com>
2024-08-05 19:58:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "60"))
 #your start default command message.
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 #your telegram tag without @
-OWNER_TAG = os.environ.get("OWNER_TAG", "cosmic_freak")
+OWNER_TAG = os.environ.get("OWNER_TAG", "Klein_Moretti_The_Fool")
 #Time in seconds for message delete
 TIME = int(os.environ.get("TIME", "60"))
 
